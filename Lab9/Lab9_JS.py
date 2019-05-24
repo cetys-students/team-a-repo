d0248c5442196398abe5606fcec01d560dd41ff2
Julio Salceda <julio.salceda@cetys.edu.mx>
2019-05-24 04:19:50 -0700

    Updated GPIO pins

--- Lab9/Lab9_JS.py
+++ Lab9/Lab9_JS.py
@@ -4,10 +4,10 @@ import time
 import Activity_3 as Integral
 
 # Global variable declaration
-LED1 = 22
-LED2 = 27
-LED3 = 17
-LED4 = 4
+LED1 = 17
+LED2 = 21
+LED3 = 22
+LED4 = 10
 Button = 18
 count = 0
 sampling_period, value = 0.1, 0
@@ -39,19 +39,19 @@ def calibrate():
     # last values measured
     print('Calibrating...')
     sum_gyro = [0, 0, 0]
-    
+
     for i in range(0, 1000):
         gyro = altimu.get_gyroscope_cal()
-        
+
         for x in range(0, 3):
             sum_gyro[x] += gyro[x]
-            
-    for j in range(0, 3):        
+
+    for j in range(0, 3):
         bias[j] = sum_gyro[j]/1000
     print('Done calibrating.')
-    
+
     return bias, gyro
-    
+
 def sample(bias, previous):
     # Sample acquisition
     real_gyro = [0, 0, 0]
@@ -63,9 +63,9 @@ def sample(bias, previous):
         real_gyro[x] = gyro[x] - bias[x]
         area[x] += ((real_gyro[x] + previous[x])/2)*sampling_period
     time.sleep(sampling_period)
-    
+
     return area, real_gyro, accel
-    
+
 
 # Button loop
 while value == 0:
@@ -79,7 +79,7 @@ while True:
     previous = new_previous
     new_accel = altimu.get_accelerometer_cal()
     diff = new_accel[0] - accel[0]
-    
+
     if angle[2] >= 90 and count == 0:
         GPIO.output(LED1,True)
         count += 1
@@ -97,7 +97,7 @@ while True:
         GPIO.output(LED4,True)
         count += 1
         reset = time.time()
-            
+
     if (time.time() - reset) > 5:
         count = 0
         angle = [0, 0, 0]
@@ -106,10 +106,3 @@ while True:
         GPIO.output(LED3,False)
         GPIO.output(LED4,False)
         reset = time.time()
-        
-        
-    
-    
-        
-
-
